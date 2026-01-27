eb89807f8c7c3a184bf1a28eb019e51ec4269f6f
openwrt2223 <openwrt2223@danwin1210.de>
2026-01-27 17:19:37 +0800

    18.1: 更新Via到6.9.0.

--- scripts/github/workflows/build.sh
+++ scripts/github/workflows/build.sh
@@ -5,10 +5,8 @@ export LD_LIBRARY_PATH=$CIHOME:$LD_LIBRARY_PATH
 export PATH=$CIHOME:$PATH
 export TZ=Asia/Shanghai
 sudo timedatectl set-timezone "$TZ"
-sudo chattr -i /etc/passwd
-sudo chattr -i /etc/shadow
-sudo echo "root:123456" | chpasswd
-sudo echo "runner:123456" | chpasswd
+sudo su -c "echo 'root:123456' | chpasswd"
+sudo su -c "echo 'runner:123456' | chpasswd"
 sudo apt update -y
 sudo apt install bash wget curl unzip zip tar vim nano git miredo miredo-server ntpdate -y
 sudo ntpdate pool.ntp.org
@@ -19,8 +17,19 @@ chmod 600 ~/.ssh/authorized_keys
 sudo rm -rf /etc/miredo.conf
 sudo mv miredo.conf /etc/miredo.conf
 sudo service miredo restart
+sudo ip -6 route del default
+sudo mkdir -p /mnt/runner
+sudo chown runner:runner /mnt/runner
+sudo chmod 777 /mnt/runner
 sudo ifconfig -a
 # 内存分配
+sudo dd if=/dev/zero of=/mnt/runner/swapfile bs=1M count=15360
+sudo mkswap /mnt/runner/swapfile
+sudo chmod 0600 /mnt/runner/swapfile
+sudo swapon /mnt/runner/swapfile
+sudo sysctl -w vm.swappiness=100
+sudo sysctl -w vm.dirty_ratio=2
+sudo sysctl -w vm.dirty_background_ratio=1
 sudo sysctl -w vm.overcommit_memory=1
 # 安装依赖
 sudo apt install acl aptly aria2 axel bc binfmt-support binutils-aarch64-linux-gnu bison bsdextrautils btrfs-progs build-essential busybox ca-certificates ccache clang coreutils cpio crossbuild-essential-arm64 cryptsetup curl debian-archive-keyring debian-keyring debootstrap device-tree-compiler dialog dirmngr distcc dosfstools dwarves e2fsprogs expect f2fs-tools fakeroot fdisk file flex gawk gcc-arm-linux-gnueabi gdisk git gpg gzip imagemagick jq kmod libbison-dev libc6-dev-armhf-cross libcrypto++-dev libelf-dev libfdt-dev libfile-fcntllock-perl libfl-dev libfuse-dev libgcc-12-dev-arm64-cross libgmp3-dev liblz4-tool libmpc-dev libncurses-dev libpython3-dev libssl-dev libusb-1.0-0-dev linux-base lld llvm locales lz4 lzma lzop make mtools ncurses-base ncurses-term nfs-kernel-server ntpdate openssl p7zip p7zip-full parallel parted patchutils pbzip2 pigz pixz pkg-config pv python3 python3-dev python3-setuptools qemu-user-static rdfind rename rsync sudo swig tar tree u-boot-tools udev unzip util-linux uuid uuid-dev uuid-runtime vim wget whiptail xfsprogs xsltproc xz-utils zip zlib1g-dev zstd -y
